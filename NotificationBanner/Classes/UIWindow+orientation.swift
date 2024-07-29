e3e41a7646c24726359f13b40132aece5470f2be
Ahmadreza Sarafraz <mail.sarafraz@gmail.com>
2024-07-29 14:19:00 +0330

    bug fix: fixed wrong banner orientation on UIWindow extension.

--- NotificationBanner/Classes/UIWindow+orientation.swift
+++ NotificationBanner/Classes/UIWindow+orientation.swift
@@ -10,27 +10,66 @@ import UIKit
 extension UIWindow {
 
     public var width: CGFloat {
-        let orientation = UIDevice.current.orientation
-        switch orientation {
-        case .landscapeLeft, .landscapeRight:
-            return max(frame.width, frame.height)
-        case .portrait, .portraitUpsideDown:
-            return min(frame.width, frame.height)
-        default:
-            return frame.width
+        if #available(iOS 13.0, *) {
+            if let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
+                let orientation = UIApplication.shared.supportedInterfaceOrientations(for: scene.windows.first)
+                switch orientation {
+                case .landscapeLeft, .landscapeRight:
+                    return max(frame.width, frame.height)
+                case .portrait, .portraitUpsideDown:
+                    return min(frame.width, frame.height)
+                default:
+                    return frame.width
+                }
+            } else {
+                return getDefaultWidth()
+            }
+        } else {
+            return getDefaultWidth()
+        }
+
+        func getDefaultWidth() -> CGFloat {
+            let orientation = UIDevice.current.orientation
+            switch orientation {
+            case .landscapeLeft, .landscapeRight:
+                return max(frame.width, frame.height)
+            case .portrait, .portraitUpsideDown:
+                return min(frame.width, frame.height)
+            default:
+                return frame.width
+            }
         }
     }
 
     public var height: CGFloat {
-        let orientation = UIDevice.current.orientation
-        switch orientation {
-        case .landscapeLeft, .landscapeRight:
-            return min(frame.width, frame.height)
-        case .portrait, .portraitUpsideDown:
-            return max(frame.width, frame.height)
-        default:
-            return frame.height
+        if #available(iOS 13.0, *) {
+            if let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
+                let orientation = UIApplication.shared.supportedInterfaceOrientations(for: scene.windows.first)
+                switch orientation {
+                case .landscapeLeft, .landscapeRight:
+                    return min(frame.width, frame.height)
+                case .portrait, .portraitUpsideDown:
+                    return max(frame.width, frame.height)
+                default:
+                    return frame.height
+                }
+            } else {
+                return getDefaultHeight()
+            }
+        } else {
+            return getDefaultHeight()
+        }
+
+        func getDefaultHeight() -> CGFloat {
+            let orientation = UIDevice.current.orientation
+            switch orientation {
+            case .landscapeLeft, .landscapeRight:
+                return min(frame.width, frame.height)
+            case .portrait, .portraitUpsideDown:
+                return max(frame.width, frame.height)
+            default:
+                return frame.height
+            }
         }
     }
-
 }
